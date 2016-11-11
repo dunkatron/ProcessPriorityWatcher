418ff16a420e7d9695d9c0652f8f3b9f957842e8
Daryl Hawkins <Daryl Hawkins>
2016-11-11 09:27:03 -0800

    First commit

---
+++ ProcessorPriorityWatcher/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+
+namespace ProcessPriorityWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                CorrectProcessWithName("Dishonored2", ProcessPriorityClass.AboveNormal);
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void CorrectProcessWithName(string processName, ProcessPriorityClass desiredPriorityClass)
+        {
+            var processes = Process.GetProcessesByName(processName);
+            if (processes.Length == 0)
+            {
+                Console.WriteLine("Did not find process {0}", processName);
+                return;
+            }
+
+            foreach (var process in processes)
+            {
+                var oldPriorityClass = process.PriorityClass;
+                if (oldPriorityClass != desiredPriorityClass)
+                {
+                    process.PriorityClass = desiredPriorityClass;
+                    Console.WriteLine("{0}: Corrected process {1} priority from {2} to {3}", DateTime.Now, process, oldPriorityClass, desiredPriorityClass);
+                }
+            }
+        }
+    }
+}
